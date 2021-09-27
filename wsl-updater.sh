0be5afee83744989f3c660ef4bc4cb6de09842a2
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-09-27 20:19:43 +0700

    ytup: fix always update even latest already installed scripts: improvements

--- wsl-updater.sh
+++ wsl-updater.sh
@@ -3,21 +3,22 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2021 rzlamrr
 
+set -e
+
 PACKAGE_DIR="/mnt/d/Linux" # use /mnt/c instead of C:\
 NEW_VERSION=$(curl --silent "https://github.com/nathanchance/WSL2-Linux-Kernel/releases/latest" | sed "s#.*tag/\(.*\)\".*#\1#")
-[[ -f ${PACKAGE_DIR}/version ]] && CURRENT_VERSION=$(cat ${PACKAGE_DIR}/version)
-[[ -f ~/bzImage ]] && rm ~/bzImage
+[[ -f ${PACKAGE_DIR}/.version ]] && CURRENT_VERSION=$(cat ${PACKAGE_DIR}/.version)
 
-if [[ "${CURRENT_VERSION}" == "" ]] || [[ "${CURRENT_VERSION}" != "${NEW_VERSION}" ]]; then
+if [[ "${CURRENT_VERSION}" != "${NEW_VERSION}" ]]; then
+    rm -f ~/bzImage
     echo "Downloading new update (${NEW_VERSION})!"
     curl --silent -L -o ~/bzImage "https://github.com/nathanchance/WSL2-Linux-Kernel/releases/download/${NEW_VERSION}/bzImage"
-    [[ -f ${PACKAGE_DIR}/bzImage ]] && mv ${PACKAGE_DIR}/bzImage ${PACKAGE_DIR}/bzImage-old
+    mv -f ${PACKAGE_DIR}/bzImage ${PACKAGE_DIR}/bzImage-old
     mv ~/bzImage ${PACKAGE_DIR}/bzImage
-    echo "${NEW_VERSION}" > ${PACKAGE_DIR}/version
+    echo "${NEW_VERSION}" > ${PACKAGE_DIR}/.version
     echo "The newest version of the WSL kernel has now been installed!"
 else
-    echo "Kernel is already at latest version ($CURRENT_VERSION)!"
+    echo "Kernel is already at latest version (${CURRENT_VERSION})!"
 fi
 
-echo "To load the new kernel, execute \"wsl.exe --shutdown\" within Windows to terminate the WSL VM."
-echo "Afterwards, restart your distro."
+echo "Restart WSL to load the new kernel!"
--- ytuploader/updater.sh
+++ ytuploader/updater.sh
@@ -3,14 +3,20 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2021 rzlamrr
 
+set -e
+
 LATEST_VERSION=$(curl --silent "https://github.com/porjo/youtubeuploader/releases/latest" | sed "s#.*tag/\(.*\)\".*#\1#")
 [[ -f ".version" ]] && CURRENT_VERSION=$(cat .version)
 
-if [[ -n "${CURRENT_VERSION}" ]] || [[ "${CURRENT_VERSION}" != "${LATEST_VERSION}" ]]; then
+if [[ "${CURRENT_VERSION}" != "${LATEST_VERSION}" ]]; then
     rm -f youtubeuploader_linux_amd64
+    echo "Downloading new update v${NEW_VERSION}!"
     curl --silent -L -o youtubeuploader_linux_amd64.tar.gz "https://github.com/porjo/youtubeuploader/releases/download/${LATEST_VERSION}/youtubeuploader_linux_amd64.tar.gz"
     tar -xzf youtubeuploader_linux_amd64.tar.gz
     rm -f youtubeuploader_linux_amd64.tar.gz
     mv -f youtubeuploader_linux_amd64 ytup
     echo ${CURRENT_VERSION} > .version
+    echo "ytuploader v${LATEST_VERSION} has been installed!"
+else
+    echo "Latest version (v${LATEST_VERSION}) already installed!"
 fi
